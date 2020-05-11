9ec3d0793f896200d54bf244cc3d2b9c0a221496
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-11 07:44:19 -0300

    Aula 18 - Saída de dados em C#

---
+++ Aula18SaidaDeDadosExercicioFixacao/Aula18SaidaDeDadosExercicioFixacao/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula18SaidaDeDadosExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Em um novo programa, inicie as seguintes variáveis:
+             * 
+             * string produto1 = "Computador";
+             * string produto2 = "Mesa de escritório";
+             * 
+             * byte idade = 30;
+             * int codigo = 5290;
+             * char genero = 'M';
+             * 
+             * double preco1 = 2100.0;
+             * double preco2 = 650.50;
+             * double medida = 53.234567;
+             * 
+             * Em seguida, usando os valores das variáveis, produza a seguinte saída na tela do console:
+             * 
+             * Produtos:
+             * Computador, cujo preço é $ 2100,00
+             * Mesa de escritório, cujo preço é $ 650,00
+             * 
+             * Registro: 30 anos de idade, código 5290 e gênero: M
+             * 
+             * Medidas com oito casas decimais: 53,23456700
+             * Arredondado (três casas decimais): 53,235
+             * Separador decimal invariant culture: 53.235
+             */
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+            Console.WriteLine("");
+            Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero: {2}", idade, codigo, genero);
+            Console.WriteLine("");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Aula18SaidasDeDados/Aula18SaidasDeDados/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula18SaidasDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+
+            // IMPRIMINDO VARIÁVEIS
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+            // PLACEHOLDER
+            Console.WriteLine("-------------------------- PLACEHOLDER");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais.", nome, idade, saldo.ToString("F2",CultureInfo.InvariantCulture));
+
+            // INTERPOLAÇÃO
+            Console.WriteLine("-------------------------- INTERPOLAÇÃO");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo.ToString("F2", CultureInfo.InvariantCulture)} reais.");
+
+            // CONCATENAÇÃO
+            Console.WriteLine("-------------------------- CONCATENAÇÃO");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais.");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+        }
+    }
+}
