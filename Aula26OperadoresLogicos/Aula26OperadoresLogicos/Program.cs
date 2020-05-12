d5f53461cf2d3a4cf15499fbf0d2a3a50e3dc15d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-12 18:18:36 -0300

    Aula 26 - Operadores Lógicos

---
+++ Aula26OperadoresLogicos/Aula26OperadoresLogicos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula26OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 && 4 != 5; // false
+            bool c2 = 2 > 3 || 4 != 5; // true
+            bool c3 = !(2 > 3) && 4 != 5; // true
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine("------------------");
+
+            bool c4 = 10 < 5;
+            bool c5 = c1 || c2 && c3 && !c4;
+
+            Console.WriteLine(c5);
+        }
+    }
+}
