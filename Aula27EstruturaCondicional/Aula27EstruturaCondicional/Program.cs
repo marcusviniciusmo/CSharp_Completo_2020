f6fec4ac07460c483e76e46352f412748cac1f3d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-13 07:31:18 -0300

    Aula 27 - Estrutura Condicional (if-else)

---
+++ Aula27EstruturaCondicional/Aula27EstruturaCondicional/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula27EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ESTRUTURA CONDICIONAL SIMPLES
+            int x = 10;
+            
+            Console.WriteLine("Bom dia");
+
+            if (x < 5)
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            
+            Console.WriteLine("Boa noite");
+
+            // ESTRUTURA CONDICIONAL COMPOSTA
+            Console.WriteLine("Entre com um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"O número {numero} é par.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é ímpar");
+            }
+        }
+    }
+}
