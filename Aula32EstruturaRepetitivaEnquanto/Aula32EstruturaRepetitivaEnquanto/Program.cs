8b780382941d1d3b97c4471485cfc3fbd0bd25fc
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-16 09:04:44 -0300

    Aula 32 - Estrutura Repetitiva Enquanto (while)

---
+++ Aula32EstruturaRepetitivaEnquanto/Aula32EstruturaRepetitivaEnquanto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula32EstruturaRepetitivaEnquanto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Digitar um número e mostrar sua raiz quadrada com três casas decimais, depois repetir o procedimento. Quando o usuário digitar um número negativo
+             * (podendo inclusive, ser na primeira vez), mostrar uma mensagem "Número negativo" e terminar o programa.
+             */ 
+
+            Console.Write("Digite um número: ");
+            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (numero >= 0.0)
+            {
+                double raizQuadrada = Math.Sqrt(numero);
+                Console.WriteLine(raizQuadrada.ToString("F3", CultureInfo.InvariantCulture));
+
+                Console.Write("Digite outro número: ");
+                numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture
+                    );
+            }
+
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}
