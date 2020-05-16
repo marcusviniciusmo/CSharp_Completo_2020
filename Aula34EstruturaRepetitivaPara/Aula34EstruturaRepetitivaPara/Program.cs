daf7b3ecb95f4a1282582d074f103e20b080e1bb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-16 19:28:26 -0300

    Aula 34 - Estrutura Repetitiva Para (for)

---
+++ Aula34EstruturaRepetitivaPara/Aula34EstruturaRepetitivaPara/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula34EstruturaRepetitivaPara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Digitar um número N e depois N valores inteiros. Mostrar a soma dos N valores digitados.
+
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+
+            int numeroDigitado, soma = 0;
+
+            for (int contador = 1; contador <= N; contador++)
+            {
+                Console.Write($"Valor #{contador}: ");
+                numeroDigitado = int.Parse(Console.ReadLine());
+
+                soma += numeroDigitado;
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
