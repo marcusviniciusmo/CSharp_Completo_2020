ab455af7c99e6075770aeed4b31a78672d308ac5
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-18 16:07:00 -0300

    Aula 41 - Criando um método para obtermos os benefícios de Reaproveitamento e Delegação

--- Aula39ResolvendoUmProblemaComOrientacaoAObjetos/Aula39ResolvendoUmProblemaComOrientacaoAObjetos/Triangulo.cs
+++ Aula39ResolvendoUmProblemaComOrientacaoAObjetos/Aula39ResolvendoUmProblemaComOrientacaoAObjetos/Triangulo.cs
@@ -1,9 +1,18 @@
-﻿namespace Aula39ResolvendoUmProblemaComOrientacaoAObjetos
+﻿using System;
+
+namespace Aula39ResolvendoUmProblemaComOrientacaoAObjetos
 {
     class Triangulo
     {
         public double A;
         public double B;
         public double C;
+
+        public double CalcularArea()
+        {
+            double p = (A + B + C) / 2.0;
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
