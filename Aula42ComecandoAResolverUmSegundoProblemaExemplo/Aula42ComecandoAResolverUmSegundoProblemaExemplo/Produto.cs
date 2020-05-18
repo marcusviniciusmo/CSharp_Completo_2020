05f3711ff5177ba3d7c48ef3e258fe689c4724bd
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-05-18 17:24:15 -0300

    Aula 42 - Começando a resolver um segundo problema exemplo

---
+++ Aula42ComecandoAResolverUmSegundoProblemaExemplo/Aula42ComecandoAResolverUmSegundoProblemaExemplo/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula42ComecandoAResolverUmSegundoProblemaExemplo
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ Aula42ComecandoAResolverUmSegundoProblemaExemplo/Aula42ComecandoAResolverUmSegundoProblemaExemplo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula42ComecandoAResolverUmSegundoProblemaExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). Em seguida:
+             * 
+             * - Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque);
+             * - Realizar uma entrada no estoque e mostrar novamente os dados do produto;
+             * - Realizar uma saída no estoque e mostrar novamente os dados do produto;
+             * 
+             * Para realizar este problema, você deve criar uma CLASSE conforme exemplo dado.
+             */
+
+            Produto produto1 = new Produto();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            produto1.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {produto1}");
+        }
+    }
+}
