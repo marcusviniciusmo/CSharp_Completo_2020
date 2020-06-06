5f8224111d113570b8c86934ff1445d297dab4eb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-06 14:44:52 -0300

    Aula 48 - Exercícios Propostos (Membros Estáticos)

---
+++ Aula48MembrosEstaticosExercicioProposto/Aula48MembrosEstaticosExercicioProposto/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula48MembrosEstaticosExercicioProposto
+{
+    class ConversorDeMoeda
+    {
+        static double IOF = 0.06; // 6% sobre o valor
+
+        public static double ConverterMoeda(double cotacao, double quantidade)
+        {
+            return (cotacao * quantidade) + IOF * cotacao * quantidade ;
+        }
+    }
+}
---
+++ Aula48MembrosEstaticosExercicioProposto/Aula48MembrosEstaticosExercicioProposto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula48MembrosEstaticosExercicioProposto
+{
+    class Program
+    {
+        /* Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais.
+         * Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá de pagar 6% de IOF sobre o 
+         * valor em dólar. Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidadeComprada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorEmReais = ConversorDeMoeda.ConverterMoeda(cotacao, quantidadeComprada);
+
+            Console.WriteLine($"Valor a ser pago em reais = {valorEmReais.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
