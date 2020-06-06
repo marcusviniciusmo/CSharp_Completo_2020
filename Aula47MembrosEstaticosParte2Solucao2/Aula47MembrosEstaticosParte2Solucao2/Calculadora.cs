ac44ae2bb75fb84bd15316c1a5afa620f9129c86
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-06 10:35:03 -0300

    Aula 47 - Membros Estáticos - Parte 2 - Solução 2

---
+++ Aula47MembrosEstaticosParte2Solucao2/Aula47MembrosEstaticosParte2Solucao2/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula47MembrosEstaticosParte2Solucao2
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3);
+        }
+    }
+}
---
+++ Aula47MembrosEstaticosParte2Solucao2/Aula47MembrosEstaticosParte2Solucao2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula47MembrosEstaticosParte2Solucao2
+{
+    class Program
+    {
+        /* Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor de uma circunferência e do volume
+         * de uma esfera para um raio daquele valor. Informar também o valor de PI com duas casas decimais.
+         * 
+         * Versão 2: classe Calculadora com membros de instância.
+         */
+
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calculadora.Circunferencia(raio);
+            double vol = calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {vol.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de Pi: {calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
