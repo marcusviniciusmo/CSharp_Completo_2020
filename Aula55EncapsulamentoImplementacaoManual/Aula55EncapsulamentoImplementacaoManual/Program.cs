cf6731789126f5ad39a8da6ee4c7f54545c75c9a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-16 10:33:37 -0300

    Aula 55 - Encapsulamento - Opção 1, Implementação Manual

---
+++ Aula55EncapsulamentoImplementacaoManual/Aula55EncapsulamentoImplementacaoManual/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula55EncapsulamentoImplementacaoManual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(produto1.GetNome());
+
+            produto1.SetNome("TV 4K");
+
+            Console.WriteLine(produto1.GetNome());
+            Console.WriteLine(produto1.GetPreco().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
