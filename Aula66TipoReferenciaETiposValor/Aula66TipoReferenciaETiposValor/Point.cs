0a1c83f865b9c5631062822e6421d1622b037fe0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-26 09:48:24 -0300

    Aula 66 - Tipos Referência e Tipos Valor

---
+++ Aula66TipoReferenciaETiposValor/Aula66TipoReferenciaETiposValor/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula66TipoReferenciaETiposValor
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Aula66TipoReferenciaETiposValor/Aula66TipoReferenciaETiposValor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula66TipoReferenciaETiposValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; // Não é necessário instanciar da seguinte forma: Point p = new Point();
+
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+            // Porém, o struct aceita o comando new
+
+            p = new Point();
+
+            Console.WriteLine(p); // Instancia um novo valor pra p.
+        }
+    }
+}
