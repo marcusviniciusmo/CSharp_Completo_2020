408148795d4999e410b4e6873e3f463fb4596bcd
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-27 15:38:54 -0300

    Aula 70 - Vetores - Parte 2

---
+++ Aula70VetoresParte2/Aula70VetoresParte2/Produtos.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula70VetoresParte2
+{
+    class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Aula70VetoresParte2/Aula70VetoresParte2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula70VetoresParte2
+{
+    class Program
+    {
+        /* Fazer um programa para ler um número inteiro N e os dados (nome e preço) de N Produtos. Armazene os N produtos em um vetor. Em seguida, mostrar o preço
+         * médio dos produtos.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de produtos: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Produtos[] prod = new Produtos[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Digite o nome do {i + 1}º produto: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Digite o preço do produto {nome}: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Antes de atribuir valores aos atributos do objeto, é necessário instanciar cada objeto da classe Produtos.
+                prod[i] = new Produtos { Nome = nome, Preco = preco}; 
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                soma += prod[i].Preco;
+            }
+
+            double media = soma / N;
+
+            Console.WriteLine($"AVERAGE PRICE = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
