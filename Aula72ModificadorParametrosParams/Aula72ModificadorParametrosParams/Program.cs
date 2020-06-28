b026f47d2901d851d5d3cfb80e476ac4b2140939
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-28 12:09:33 -0300

    Aula 72 - Modificador de Parâmetros params

---
+++ Aula72ModificadorParametrosParams/Aula72ModificadorParametrosParams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula72ModificadorParametrosParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 3);
+            int result = Calculator.Sum(10, 20, 30, 40);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(result);
+        }
+    }
+}
