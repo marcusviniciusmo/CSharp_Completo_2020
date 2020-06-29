2c5852b95ff94e2e94b3cf7e919d1c4d302dc776
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-29 17:16:59 -0300

    Aula 75 - Sintaxe Opcional: Laço Foreach

---
+++ Aula75SintaxeOpcionalLacoForeach/Aula75SintaxeOpcionalLacoForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula75SintaxeOpcionalLacoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach(string nome in vetor)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
