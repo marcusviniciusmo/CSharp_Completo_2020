e39bacae8ab6d7b450093bf965d86dd771664394
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-29 16:05:44 -0300

    Aula 73 - Modificadores de Parâmetros Ref e Out (modificador ref)

---
+++ Aula73ModificadorParametroRef/Aula73ModificadorParametroRef/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula73ModificadorParametroRef
+{
+    class Calculator
+    {
+        public static void FuncaoQueNaoTriplica(int x)
+        {
+            x = x * 3;
+        }
+
+        public static void FuncaoQueTriplica(ref int x)
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ Aula73ModificadorParametroRef/Aula73ModificadorParametroRef/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula73ModificadorParametroRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            // Chamada de uma função que não triplica o valor passado como parâmetro.
+            Calculator.FuncaoQueNaoTriplica(a);
+
+            Console.WriteLine("Função que não triplica o valor passado como parâmetro.");
+            Console.WriteLine(a);
+
+            // Chamada de uma função que triplica o valor passado como parâmetro.
+            Calculator.FuncaoQueTriplica(ref a);
+            
+            Console.WriteLine("Função que triplica o valor passado como parâmetro.");
+            Console.WriteLine(a);
+        }
+    }
+}
