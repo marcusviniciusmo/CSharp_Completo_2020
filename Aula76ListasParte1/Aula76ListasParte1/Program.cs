8b3ef23ebdf47159702ede9aea01017988ee5c0e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-29 20:02:33 -0300

    Aula 76 - Listas (List) - Parte 1

---
+++ Aula76ListasParte1/Aula76ListasParte1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula76ListasParte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista1; // Criação de uma lista.
+
+            List<string> lista2 = new List<string>(); // Criação e instanciação de uma lista.
+
+            List<string> lista3 = new List<string>() { "Maria", "Alex", "Bob" }; // Criação, instanciação e atribuição de uma lista.
+        }
+    }
+}
