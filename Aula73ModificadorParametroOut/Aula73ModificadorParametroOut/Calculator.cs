f8bf5a8e5c543d305efba7582d693fff2d6db514
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-29 16:52:40 -0300

    Aula 73 - Modificadores de Parâmetros Ref e Out (modificador out)

---
+++ Aula73ModificadorParametroOut/Aula73ModificadorParametroOut/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula73ModificadorParametroOut
+{
+    class Calculator
+    {
+        public static void Triplicar(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Aula73ModificadorParametroOut/Aula73ModificadorParametroOut/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula73ModificadorParametroOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+            Calculator.Triplicar(a, out triple);
+
+            Console.WriteLine(triple);
+        }
+    }
+}
