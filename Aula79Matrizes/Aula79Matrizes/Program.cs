c696ac530c4caf2e8ea4c33426add9f852a2fb8d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-06-30 11:26:49 -0300

    Aula 79 - Matrizes

---
+++ Aula79Matrizes/Aula79Matrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula79Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = new double[2, 3];
+
+            Console.WriteLine("---------- COMANDO Lenght ----------");
+            Console.WriteLine($"Quantos elementos a matriz tem no total: {matriz.Length}");
+
+            Console.WriteLine("---------- COMANDO Rank ----------");
+            Console.WriteLine($"A quantidade de linhas da matriz: {matriz.Rank}");
+
+            Console.WriteLine("---------- COMANDO GetLenght ----------");
+            Console.WriteLine($"A dimensão {0} tem tamanho {matriz.GetLength(0)}"); // Quantidade de linhas
+
+            Console.WriteLine($"A dimensão {1} tem tamanho {matriz.GetLength(1)}"); // Quantidade de colunas
+        }
+    }
+}
