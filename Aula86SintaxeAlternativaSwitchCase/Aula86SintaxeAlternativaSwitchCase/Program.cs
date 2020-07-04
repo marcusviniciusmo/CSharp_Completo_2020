c16a7403c6b011115574df3fe3a1b40c9c078c15
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-07-04 10:47:04 -0300

    Aula 86 - Sintaxe Alternativa: Switch-Case

---
+++ Aula86SintaxeAlternativaSwitchCase/Aula86SintaxeAlternativaSwitchCase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aula86SintaxeAlternativaSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the day of week: ");
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            string day;
+
+            switch (dayOfWeek)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+
+            Console.WriteLine($"Day of Week: {day}");
+        }
+    }
+}
