b38f9589b7ad2abdbd6e86517cc261504b1f27e6
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-07-04 10:11:42 -0300

    Aula 85 - Inferência de Tipos: Palavra var

---
+++ Aula85InferenciaDeTiposPalavraVar/Aula85InferenciaDeTiposPalavraVar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula85InferenciaDeTiposPalavraVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10; // inteiro
+            var y = 2.5; // double
+            var z = 4.5f; // float
+            var a = "Maria"; // string
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+        }
+    }
+}
