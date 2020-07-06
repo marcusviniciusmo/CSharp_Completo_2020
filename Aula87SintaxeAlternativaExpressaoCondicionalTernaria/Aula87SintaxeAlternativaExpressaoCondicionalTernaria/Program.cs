5b2d55c24c4a9d8bdbb575494d425389bfc8f6e8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-07-06 04:34:16 -0300

    Aula 87 - Sintaxe Alternativa - Expressão Condicional Ternária

---
+++ Aula87SintaxeAlternativaExpressaoCondicionalTernaria/Aula87SintaxeAlternativaExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula87SintaxeAlternativaExpressaoCondicionalTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double discount = (price > 20.0) ? price * 0.10 : price * 0.05;
+
+            Console.WriteLine($"Dicount: US$ {discount.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
+ 
