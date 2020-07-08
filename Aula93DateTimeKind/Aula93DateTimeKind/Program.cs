eeeebdc79d5854b1032efa051e52a90b3a1cc193
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2020-07-08 20:31:21 -0300

    Aula 93 - DateTimeKind e padrão ISO 8601 - DateTimeKind

---
+++ Aula93DateTimeKind/Aula93DateTimeKind/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula93DateTimeKind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            Console.WriteLine("---------- CONVERSÕES LOCAL E UTC - ----------");
+            Console.WriteLine($"d1: {d1}");
+            Console.WriteLine($"d1 kind: {d1.Kind}");
+            Console.WriteLine($"d1 to Local: {d1.ToLocalTime()}");
+            Console.WriteLine($"d1 to Utc: {d1.ToUniversalTime()}");
+
+            Console.WriteLine();
+            Console.WriteLine($"d2: {d2}");
+            Console.WriteLine($"d2 kind: {d2.Kind}");
+            Console.WriteLine($"d2 to Local: {d2.ToLocalTime()}");
+            Console.WriteLine($"d2 to Utc: {d2.ToUniversalTime()}");
+
+            Console.WriteLine();
+            Console.WriteLine($"d3: {d3}");
+            Console.WriteLine($"d3 kind: {d3.Kind}");
+            Console.WriteLine($"d3 to Local: {d3.ToLocalTime()}");
+            Console.WriteLine($"d3 to Utc: {d3.ToUniversalTime()}");
+        }
+    }
+}
