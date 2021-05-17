c1de1fa5ac5bac549e41b289aa860ce274419dc8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-17 14:22:07 -0300

    Aula 115 - Enumerações

---
+++ Aula115Enumeracoes/Aula115Enumeracoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula115Enumeracoes.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        // Caso não seja atribuído nenhum valor, o C# faz uma contagem começando com o valor 0 (zero).
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Aula115Enumeracoes/Aula115Enumeracoes/Program.cs
@@ -0,0 +1,38 @@
+﻿using Aula115Enumeracoes.Entities;
+using Aula115Enumeracoes.Entities.Enums;
+using System;
+
+namespace Aula115Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine("ENUMERAÇÕES");
+            Console.WriteLine(order);
+
+            // CONVERSÃO DE ENUMERAÇÃO PARA STRING
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine();
+            Console.WriteLine("CONVERSÃO DE ENUMERAÇÃO PARA STRING");
+            Console.WriteLine(txt);
+
+            // CONVERSÃO DE STRING PARA ENUMERAÇÃO
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            
+            Console.WriteLine();
+            Console.WriteLine("CONVERSÃO DE STRING PARA ENUMERAÇÃO");
+            Console.WriteLine(os);
+        }
+    }
+}
