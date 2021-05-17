a8de6fee5074e1bbcf7ad0052ee836dd59524584
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-17 17:35:50 -0300

    Aula 120 - Exercício Resolvido 01 - Parte 2/3

---
+++ Aula119ExercicioResolvido1Parte1-3/Aula119ExercicioResolvido1Parte1-3/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula119ExercicioResolvido1Parte1_3.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Aula119ExercicioResolvido1Parte1-3/Aula119ExercicioResolvido1Parte1-3/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula119ExercicioResolvido1Parte1_3.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior,
+        MidLevel,
+        Senior
+    }
+}
