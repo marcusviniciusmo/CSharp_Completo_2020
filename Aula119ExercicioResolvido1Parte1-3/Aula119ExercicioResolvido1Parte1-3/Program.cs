a8c25bbd8c8169d4f3d2aae1fb326238376141b9
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-17 17:30:27 -0300

    Aula 119 - Exercício Resolvido 01 - Parte 1/3

---
+++ Aula119ExercicioResolvido1Parte1-3/Aula119ExercicioResolvido1Parte1-3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula119ExercicioResolvido1Parte1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar do usuário um mês 
+             * e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo.
+             */
+        }
+    }
+}
