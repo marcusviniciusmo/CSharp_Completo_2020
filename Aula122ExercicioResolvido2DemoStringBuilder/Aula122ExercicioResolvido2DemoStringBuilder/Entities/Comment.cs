e740093fc6cfaac638aeb265c0db2c908973fbca
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-18 16:43:01 -0300

    Aula 122 - Exercício Resolvido 02 (demo StringBuilder)

---
+++ Aula122ExercicioResolvido2DemoStringBuilder/Aula122ExercicioResolvido2DemoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula122ExercicioResolvido2DemoStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
