b679631d4a246af9bb14643746e721b65ea35af2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-31 20:25:02 -0300

    Aula 143 - Criando Exceções Personalizadas - Parte 1

---
+++ Aula143CriandoExcecoesPersonalizadasParte1/Aula143CriandoExcecoesPersonalizadasParte1/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Aula143CriandoExcecoesPersonalizadasParte1.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append($"Reservation: Room {RoomNumber}, ");
+            str.Append($"check-in: {CheckIn.ToString("dd/MM/yyyy")}, ");
+            str.Append($"check-out: {CheckOut.ToString("dd/MM/yyyy")}, ");
+            str.AppendLine($"{Duration()} nights");
+
+            return str.ToString();
+        }
+    }
+}
---
+++ Aula143CriandoExcecoesPersonalizadasParte1/Aula143CriandoExcecoesPersonalizadasParte1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Aula143CriandoExcecoesPersonalizadasParte1.Entities;
+
+namespace Aula143CriandoExcecoesPersonalizadasParte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data de entrada e data de saída)
+             * e mostrar os dados da reserva, inclusive sua duração em dias. Em seguida, ler novas datas de entrada e saída,
+             * atualizar a reserva, e mostrar novamente a reserva com os dados atualizados. O programa não deve aceitar dados 
+             * inválidos para a reserva, conforme as seguintes regras:
+             * 
+             * Alterações de reserva só podem ocorrer para datas futuras.
+             * A data de saída deve ser maior que a data de entrada.
+             */
+
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out data (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine(reservation.ToString());
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out data (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine(reservation.ToString());
+                }
+            }
+        }
+    }
+}
