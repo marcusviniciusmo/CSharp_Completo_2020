7e273480078b09c04cf9e83400e09ae9e872733a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-31 10:38:29 -0300

    Aula 141 - Estrutura Try-Catch

---
+++ Aula141EstruturaTryCatch/Aula141EstruturaTryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula141EstruturaTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("N1: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("N2: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                int result = n1 / n2;
+                Console.Write($"Result N1 / N2: {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error! {e.Message}");
+            }
+        }
+    }
+}
