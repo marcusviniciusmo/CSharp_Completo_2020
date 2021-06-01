98efa310d8fb80e1bd55dffbeb2b472c45647e9d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-05-31 22:02:11 -0300

    Aula 146 - Exercício Proposto (Exceções)

---
+++ Aula146ExercicioPropostoExcecoes/Aula146ExercicioPropostoExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aula146ExercicioPropostoExcecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
---
+++ Aula146ExercicioPropostoExcecoes/Aula146ExercicioPropostoExcecoes/Program.cs
@@ -0,0 +1,53 @@
+﻿using Aula146ExercicioPropostoExcecoes.Entities;
+using Aula146ExercicioPropostoExcecoes.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace Aula146ExercicioPropostoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta bancária,
+             * mostrando o novo saldo. Um saque não pode ocorrer ou se não houver saldo na conta, ou se o valor do saque for
+             * superior ao limite de saque da conta. Implemente a conta bancária conforme projeto.
+             */
+
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter the amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                acc.Withdraw(withdraw);
+
+                Console.WriteLine($"New balance: {acc.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Withdraw error: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+        }
+    }
+}
