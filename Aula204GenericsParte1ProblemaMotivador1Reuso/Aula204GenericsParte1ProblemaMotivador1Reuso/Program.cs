24555bd9fc82a3fdb106f48aa170ec45011b6589
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-09-26 10:35:49 -0300

    Aula 204 - Generics - Parte 1

---
+++ Aula204GenericsParte1ProblemaMotivador1Reuso/Aula204GenericsParte1ProblemaMotivador1Reuso/Program.cs
@@ -0,0 +1,32 @@
+﻿using Aula204GenericsParte1ProblemaMotivador1Reuso.Services;
+using System;
+
+namespace Aula204GenericsParte1ProblemaMotivador1Reuso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Deseja-se fazer um programa que leia um conjunto de N números inteiros (N de 1 a 10),
+             * e depois imprima esses números de forma organizada conforme exemplo.
+             * Em seguida, informar qual foi o primeiro valor informado.
+             */
+
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
