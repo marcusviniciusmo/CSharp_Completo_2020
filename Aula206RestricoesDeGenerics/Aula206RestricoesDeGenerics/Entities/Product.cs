c270c1f8522423975ec024ae779e4f524d868379
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-10-05 18:25:18 -0300

    Aula 206 - Restrições de Generics

---
+++ Aula206RestricoesDeGenerics/Aula206RestricoesDeGenerics/Entities/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Aula206RestricoesDeGenerics.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append($"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return str.ToString();
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not a Product");
+            }
+
+            Product otherProduct = obj as Product;
+
+            return Price.CompareTo(otherProduct.Price);
+        }
+    }
+}
---
+++ Aula206RestricoesDeGenerics/Aula206RestricoesDeGenerics/Program.cs
@@ -0,0 +1,42 @@
+﻿using Aula206RestricoesDeGenerics.Entities;
+using Aula206RestricoesDeGenerics.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Aula206RestricoesDeGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Uma empresa de consultoria deseja avaliar a performance de produtos, funcionários,
+             * dentre outras coisas. Um dos cálculos que ela precisa é encontrar o maior dentre um
+             * conjunto de elementos. Fazer um programa que leia um conjunto de N produtos,
+             * conforme exemplo, e depois mostre o mais caro deles.
+             */
+
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(',');
+                string name = vet[0];
+                double price = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(max);
+        }
+    }
+}
