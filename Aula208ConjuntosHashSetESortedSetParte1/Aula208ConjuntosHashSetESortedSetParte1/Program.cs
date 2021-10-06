7642427aea05230516abf832701ab1c3ec4031d1
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-10-06 08:36:27 -0300

    Aula 208 - Conjuntos: HashSet e SortedSet - Parte 1

---
+++ Aula208ConjuntosHashSetESortedSetParte1/Aula208ConjuntosHashSetESortedSetParte1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula208ConjuntosHashSetESortedSetParte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Computer"));
+
+            Console.WriteLine();
+
+            foreach (string hashSet in set)
+            {
+                Console.WriteLine(hashSet);
+            }
+        }
+    }
+}
