5efb417eb6b38bbf9048ba1b7833f67181950279
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-10-31 16:00:24 +0100

    Aula 211 - Exercício Resolvido sobre Conjuntos

---
+++ Aula211ExercicioResolvidoSobreConjuntos/Aula211ExercicioResolvidoSobreConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula211ExercicioResolvidoSobreConjuntos.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord otherLogRecord = obj as LogRecord;
+
+            return Username.Equals(otherLogRecord.Username);
+        }
+    }
+}
---
+++ Aula211ExercicioResolvidoSobreConjuntos/Aula211ExercicioResolvidoSobreConjuntos/Program.cs
@@ -0,0 +1,46 @@
+﻿using Aula211ExercicioResolvidoSobreConjuntos.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aula211ExercicioResolvidoSobreConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Um site de internet registra um log de acessos dos usuários. Um registro de log consiste
+             * no nome de usuário e o instante em que o usuário acessou o site no padrão ISO 8601, separados por espaço,
+             * conforme exemplo. Fazer um programa que leia o log de acessos a partir de um arquivo,
+             * e daí informe quantos usuários distintos acessaram o site.
+             */
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+
+                    Console.WriteLine($"Total users: {set.Count}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
