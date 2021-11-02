33eb75d2cb35070966366288679f2df1fb4b1deb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-11-02 17:27:02 +0100

    Aula 213 - Dictionary e SortedDictionary

---
+++ Aula213DictionaryESortedDictionary/Aula213DictionaryESortedDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula213DictionaryESortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+            cookies["phone"] = "83737388";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+            Console.WriteLine();
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Size: {cookies.Count}");
+
+            Console.WriteLine();
+            Console.WriteLine("All Cookies:");
+
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
