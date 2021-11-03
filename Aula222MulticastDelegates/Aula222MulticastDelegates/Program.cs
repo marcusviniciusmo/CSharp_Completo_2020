69f75c2903954b65b38bd6912e7f25c6b41d1f09
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-11-03 16:06:35 +0100

    Aula 222 - Multicast delegates

---
+++ Aula222MulticastDelegates/Aula222MulticastDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using Aula222MulticastDelegates.Services;
+using System;
+
+namespace Aula222MulticastDelegates
+{
+    class Program
+    {
+        delegate void BianryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            Console.WriteLine($"A: {a}");
+            Console.WriteLine($"B: {b}");
+            Console.WriteLine();
+
+            BianryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
